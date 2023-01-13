03d3e9a9b8c2a10338dfa07a6eb6b811340f533c
Athena Serene <aserenehoosang@gmail.com>
2023-01-12 17:54:54 -0800

    added Header to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Outlet />
+      <Header/>
     </div>
   );
 }
