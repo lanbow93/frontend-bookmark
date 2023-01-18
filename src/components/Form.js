14a4a846f1f06d3a7257f37ba84c988fc2d4798e
lanbow93 <the.doctor1501@gmail.com>
2023-01-17 20:55:22 -0600

    new bookmark css

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,13 @@ import {Form} from 'react-router-dom'
 
 function NewForm(props){
     return(
-        <Form action="/create" method="post">
+
+        <Form action="/create" method="post" className='formField'>
+            <label>Name: </label>
             <input type="input" name="title" placeholder="bookmark" />
+            <label>URL: </label>
             <input type="input" name="url" placeholder="http://" />
-            <input type="submit" value="Add" />
+            <input type="submit" value="Add Bookmark" />
         </Form>
     )
 }
